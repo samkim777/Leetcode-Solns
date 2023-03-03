4a55388e57c17bebce0882a484e8198418da2473
samkim <milk3531@hotmail.com>
2023-03-02 21:39:29 -0800

    Key: assign none to visited vals, and if we see again then it is a cycle

---
+++ Solutions/Easy/141_Linked_List_Cycle.py
@@ -0,0 +1,17 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        cur = head
+        while(cur):
+            if(cur.val is None):
+                return True
+            cur.val = None
+            cur = cur.next
+
+        return False    
+
