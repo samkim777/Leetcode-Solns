36de218d68c17ff4fb7865f801f07e4c28297c93
samkim <milk3531@hotmail.com>
2023-03-08 19:47:08 -0800

    Key: Keeping track of what the left multiples are and what the right multiples are

--- Solutions/Medium/238 product of array except self.py
+++ Solutions/Medium/238 product of array except self.py
@@ -1,12 +1,18 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        res = [1] * len(nums)
-        prefix = 1
-        for i in range(len(nums)):
-            res[i] = prefix
-            prefix *= nums[i]
-        postfix = 1
-        for j in range(len(nums) - 1, - 1, - 1):
-            res[j] *= postfix
-            postfix *= nums[j]
-        return res        
+        n = len(nums)
+        left_products = [1] * n
+        right_products = [1] * n
+        output = [1] * n
+        left_products[0] = 1
+        right_products[n-1] = 1
+
+        for i in range(1,len(nums)):
+            left_products[i] = left_products[i-1] * nums[i-1]
+        for j in range(len(nums) - 2,-1,-1):
+            right_products[j] = right_products[j+1] * nums[j+1]
+
+        for k in range(0,len(nums)):
+            output[k] = left_products[k] * right_products[k]
+
+        return output                    
