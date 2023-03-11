be6f7c998343f511bdb51a387aaadc612aff98be
samkim <milk3531@hotmail.com>
2023-03-10 17:29:52 -0800

    Key: Floyd algorithm. Traversing one pointer at twice the speed, and checking first intersections. Then traversing another slow pointer and return the start of linked list cycle

--- Solutions/Medium/287. Find the Duplicate Number.py
+++ Solutions/Medium/287. Find the Duplicate Number.py
@@ -1,1 +1,18 @@
-# Floyd mayweather algorithm wut
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        slow,fast = 0,0
+
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]] # Traverse twice as fast
+            if slow == fast:
+                break
+
+        slow2 = 0
+
+        while True:
+            slow = nums[slow] # Slow pointer starts at the first intersection
+            slow2 = nums[slow2]
+            if slow == slow2:
+                return slow2        
+                     
