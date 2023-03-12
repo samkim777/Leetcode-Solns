5944cf2df35106c3d04cc6bc59259a57549ba11a
samkim <milk3531@hotmail.com>
2023-03-11 20:57:15 -0800

    Key: Using n-1 to check start of sequence, then using for and while loops to achieve O(n) complexity

--- Solutions/Medium/128 longest consecutive sequence2.py
+++ Solutions/Medium/128 longest consecutive sequence2.py
@@ -1,13 +1,13 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        numSet = set(nums)
+        numSet = set(nums) # [100,4,200,1,3,2] to {100,4,200,1,3,2}
+                           # If duplicates, [0,0,2,3] -> [0,2,3]
         longest = 0
 
         for n in nums:
-            # Check if start of a sequence 
-            if (n - 1) not in numSet: 
-                length = 0
-                while (n + length) in numSet:
+            if (n - 1) not in numSet: # Start of sequence
+                length = 0  
+                while (n + length in numSet): #O(s), where s = length
                     length+=1
-                longest = max(length,longest)
-        return longest        
+                longest = max(length,longest) # Keep track of longest
+        return longest             
