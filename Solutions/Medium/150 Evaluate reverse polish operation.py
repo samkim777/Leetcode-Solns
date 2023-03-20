59ea12b008151b6eb189d432015c3f268d8c7477
samkim <milk3531@hotmail.com>
2023-03-19 21:36:29 -0700

    Key: Using stack.pop returns and removes values, checking operators to strings is easy

--- Solutions/Medium/150 Evaluate reverse polish operation.py
+++ Solutions/Medium/150 Evaluate reverse polish operation.py
@@ -1,17 +1,24 @@
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
+        # First thing is how to check +,-,*,/? 
+        # Test each case by checking if +, then add, etc..
+        # ["2","1","+","3","*"] is (2 + 1) * 3
+        # Using stacks: Put everything into stack
+        # If we've got an operator, replace the last 2 numerical values
+        # with the number1 operand number2 value
+        # Expect we won't have empty lists
         stack = []
-        for s in tokens:
-            if s == "+":
-                stack.append(stack.pop()+stack.pop())
-            elif s == "-":
+        for c in tokens:
+            if c == '+':
+                stack.append(stack.pop() + stack.pop()) # Pop the first two numbers
+            elif c == '-':
                 a,b = stack.pop(),stack.pop()
-                stack.append(b-a)
-            elif s == "*":
-                stack.append(stack.pop() * stack.pop())
-            elif s == "/":     
-                a,b = stack.pop(),stack.pop()
-                stack.append(int(b/a))
-            else:
-                stack.append(int(s))        
-        return stack[0]
+                stack.append(b - a) 
+            elif c == '*':
+                stack.append( stack.pop() * stack.pop())
+            elif c == '/':
+                a,b = stack.pop(), stack.pop()
+                stack.append(int(b/a)) 
+            else: 
+                stack.append(int(c)) # Number    
+        return stack[0] 
