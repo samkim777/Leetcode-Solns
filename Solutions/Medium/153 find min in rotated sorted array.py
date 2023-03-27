41c21b41d58836d7a36a1d4ec28c923dd630eb7d
samkim <milk3531@hotmail.com>
2023-03-26 22:27:58 -0700

    Key: If first element less than last, then initial array. Else, pick the side of the list that is increasing to find the minimum

--- Solutions/Medium/153 find min in rotated sorted array.py
+++ Solutions/Medium/153 find min in rotated sorted array.py
@@ -1,16 +1,25 @@
 class Solution:
     def findMin(self, nums: List[int]) -> int:
-        res = nums[0]
+        # Initial list is sorted in ascending order
+        # Rotated 1 to n times, so always rotated at least once
+        # Rotating by n times resets it to the inital list
+        # [1,2,3,4,5] [5,2,3,4,1]
+        # case where we rotated n times, and have the initial list
+        # We will treat this case in special case
+        # [3,4,5,1,2] Compare 3 and 5 
+        # Since 5 is bigger than 3, it must be that up until this middle point, we must be increasing. And so search the right side of the list
+        # [5,1,2,3,4] 2 is less than 5, so we move r pointer to mid - 1, ie
+        # search the left side of the list
         l,r = 0, len(nums) - 1
-
-        while(l<=r):
-            if nums[l] < nums[r]:
+        res = nums[0]
+        while(l <= r):
+            if nums[l] < nums[r]: # Initial array
                 res = min(res,nums[l])
                 break
-            m = (l+r) // 2
-            res = min(res,nums[m])
-            if nums[m] >= nums[l]: # What's the logic behind this?
-                l = m + 1
-            else: 
-                r = m - 1
-        return res                
+            mid = (l + r) // 2
+            res = min(res,nums[mid])
+            if nums[mid] >= nums[l]: # If 2 elements, then rounds to 0, so need equal check 
+                l = mid + 1 # l = nums[4] == 0
+            else:
+                r = mid - 1
+        return res      
