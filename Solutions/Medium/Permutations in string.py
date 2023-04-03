6752364b1f8a2f463333d2f9ec1b4fd198d65010
samkim <milk3531@hotmail.com>
2023-04-03 14:48:25 -0700

    Key: Added 2 other solutions, one optimal using ASCII and hashmap, the other using collections.Counter() in a more effective manner

--- Solutions/Medium/Permutations in string.py
+++ Solutions/Medium/Permutations in string.py
@@ -14,7 +14,56 @@ class Solution:
             r += 1                                   
             if all([countS1[i] == 0 for i in countS1]):
                 return True
-        return False                   
+        return False 
+
+    # Faster solution without duplicate Counter()     
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        l,r = 0,len(s1)
+        countS1 = collections.Counter(s1) 
+        while r <= len(s2):
+          if countS1 == collections.Counter(s2[l:r]):
+            return True
+          l+= 1
+          r+= 1
+        return False    
+
+    # optimal solution using ASCII Values
+
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1) > len(s2): return False
+
+        s1Count, s2Count = [0] * 26, [0] * 26 #initialize hashmap length 26
+        for i in range(len(s1)):
+            s1Count[ord(s1[i]) - ord('a')] += 1
+            s2Count[ord(s2[i]) - ord('a')] += 1 
+
+        matches = 0
+
+        for i in range(26):
+            matches += (1 if s1Count[i] == s2Count[i] else 0)
+
+        l = 0
+
+        for r in range(len(s1), len(s2)):
+            if matches == 26: return True
+
+            index = ord(s2[r]) - ord('a') # Key of right pointer 
+            s2Count[index] += 1 
+            if s1Count[index] == s2Count[index]:
+                matches += 1
+            elif s1Count[index] + 1 == s2Count[index]:
+                matches -= 1
+
+            index = ord(s2[l]) - ord('a') # Key of left pointer
+            s2Count[index] -= 1
+            if s1Count[index] == s2Count[index]:
+                matches += 1
+            elif s1Count[index] - 1 == s2Count[index]:
+                matches -= 1 
+            l += 1
+
+        return matches == 26     
+                                                 
 
                 
                     
