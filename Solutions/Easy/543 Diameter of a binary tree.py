9deeef2cdc60d5793b6451d79142a86d2a46d6be
samkim <milk3531@hotmail.com>
2023-04-19 13:26:01 -0700

    Key: Defining another function that will recursively find the depth of left and right maximum depths

---
+++ Solutions/Easy/543 Diameter of a binary tree.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        self.max_diameter = 0
+        self.getDiameter(root)
+        return self.max_diameter
+
+
+
+    def getDiameter(self,root):
+        if not root:
+            return 0
+
+        left_depth = self.getDiameter(root.left)
+        right_depth = self.getDiameter(root.right)
+
+        diameter = left_depth + right_depth
+        self.max_diameter = max(self.max_diameter,diameter)
+        return 1 + max(left_depth,right_depth)           
