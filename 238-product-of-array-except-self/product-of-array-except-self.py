699c49d0073ed7fb250d007ea5c23ea1a8741e0e
Sam <51801821+samkim777@users.noreply.github.com>
2025-01-03 16:24:12 -0800

    Time: 23 ms (64.93%) | Memory: 23.5 MB (39.66%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -3,17 +3,15 @@ class Solution:
         # prefix[i] = nums[i-1] * prefix[i-1]
         # postfix[i] = nums[i+1] * postfix[i+1]
         # result[i] = prefix[i] * postfix[i]
+        # O(n) time, O(n) space
         nLen = len(nums)
-        postfix, prefix = [1] * nLen, [1] * nLen
+        prefix = [1] * nLen
         for i in range(1,len(nums)):
             prefix[i] = prefix[i - 1] * nums[i - 1]
 
-        # postfix
-        for j in range(len(nums) - 2, -1, -1): # R -> L
-            postfix[j] = postfix[j + 1] * nums[j + 1]
+        postfix = 1    
+        for k in range(len(nums) - 1, - 1, -1):
+            prefix[k] *= postfix
+            postfix *= nums[k]
 
-        result = []    
-        for k in range(len(nums)):
-            cur = postfix[k] * prefix[k]
-            result.append(cur)
-        return result    
+        return prefix
