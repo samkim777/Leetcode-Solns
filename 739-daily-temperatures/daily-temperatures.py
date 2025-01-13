195c2618ed4aa40b24d44ba65a4bfa978ddff3c9
Sam <51801821+samkim777@users.noreply.github.com>
2025-01-12 17:35:27 -0800

    Time: 141 ms (18.05%) | Memory: 35.4 MB (5.14%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.py
+++ 739-daily-temperatures/daily-temperatures.py
@@ -1,12 +1,11 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        # Use monotonic stack
         res = [0] * len(temperatures)
-        stack = [] # [temp, index]
+        stack = [] # [temp,index]
 
-        for i, t in enumerate(temperatures):
-            while stack and stack[-1][0] < t: # Compare top of stack with current temp
-                stackTemp, stackIndex = stack.pop()
-                res[stackIndex] = i - stackIndex
+        for i,t in enumerate(temperatures):
+            while stack and stack[-1][0] < t:
+                stackT, stackI = stack.pop()
+                res[stackI] = i - stackI
             stack.append([t,i])
         return res        
