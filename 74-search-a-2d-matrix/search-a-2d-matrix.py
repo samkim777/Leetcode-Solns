2bc3e7ca5def48e1e8514d33a490ecfb055bebd5
Sam <51801821+samkim777@users.noreply.github.com>
2025-01-14 21:58:43 -0800

    Time: 0 ms (100.00%) | Memory: 18.2 MB (35.45%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -1,22 +1,30 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        # Go through each row
-        # if l <= target <= r, search this row
-        # Else continue
-        # return True if found, false else
-        # Going through each row, so is that O(m)?
-        l,r = 0, len(matrix[0]) - 1
-        for row in matrix:
-            if target <= row[r] and target >= row[l]:
-                # search this row
-                while l <= r:
-                    mid = (l + r) // 2
-                    if row[mid] == target:
-                        return True
-                    elif row[mid] > target:
-                        r = mid - 1
-                    else:
-                        l = mid + 1               
+        # O(logn) + O(logm) Time complexity
+        # O(1) space
+        ROW, COL = len(matrix), len(matrix[0])
+        top, bot = 0, ROW - 1
+        while top <= bot:
+            row = (top + bot) // 2
+            if target > matrix[row][-1]: # Bigger than biggest in that row
+                top = row + 1
+            elif target < matrix[row][0]: # smaller than smallest
+                bot = row - 1
+            else: # In this current row
+                break
+
+        # If we exit while loop above but no found, target no existe
+        if not (top <= bot):
+            return False
+        # Now loop through our found row
+        row = (top + bot) // 2
+        l,r = 0, COL - 1
+        while l <= r:
+            mid = (l+r) // 2
+            if matrix[row][mid] == target:
+                return True
+            elif matrix[row][mid] > target:
+                r = mid - 1
             else:
-                continue
-        return False            
+                l = mid + 1
+        return False                                
