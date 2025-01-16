89e4485298b3cd347abe14a0af65eb15969a64d9
Sam <51801821+samkim777@users.noreply.github.com>
2025-01-15 21:52:12 -0800

    Time: 1 ms (45.65%) | Memory: 18.1 MB (23.55%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.py
+++ 22-generate-parentheses/generate-parentheses.py
@@ -1,19 +1,21 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        # add Open if # open < n
-        # add closed if # closed < open
-        # valid IIF open == closed == n
-        # Time: O(4n/sqrt(n)), space: O(n)
+        # Conditions for backtracking:
+        # 1) # Open Brackets < n
+        # 2) # Closed Brackets < # Open Brackets
+        # 3) Valid IIF # Closed Brackets == # Opem Brackets
+        # Time: O(4n/sqrtn), Space: O(n)?
         res = []
-        def backtrack(openN, closedN, sPath):
-            # Base
+        def backtrack(openN, closedN, sCur):
             if openN == closedN == n:
-                res.append(sPath)
-            # Open < n
+                res.append(sCur)
+                return
+
             if openN < n:
-                backtrack(openN + 1, closedN, sPath+"(")    
-             # closed < open
+                backtrack(openN + 1, closedN, sCur+"(")
+
             if closedN < openN:
-                backtrack(openN, closedN + 1, sPath+")")
+                backtrack(openN, closedN+1, sCur+")") 
+
         backtrack(0,0,"")
-        return res 
+        return res           
