a259148d70d16bfefb9b5b2f4000a04d470b048b
Sam <51801821+samkim777@users.noreply.github.com>
2025-01-21 20:35:57 -0800

    Update file 1/21/2025, 8:35:57 PM

--- 121-BestTimetoBuyandSellStock/121-BestTimetoBuyandSellStock.py
+++ 121-BestTimetoBuyandSellStock/121-BestTimetoBuyandSellStock.py
@@ -1,12 +1,17 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        l,r = 0,1
-        maxprofit = 0
-
+        # We can use two pointers / sliding window
+        # If the left pointer >= right, increment left
+        # If the right pointer > left, calculate current price, decrement right
+        # return the result
+        # O(n) time complexity, O(1) space, no extra used
+        res = 0
+        l,r = 0, 1
         while r < len(prices):
             if prices[l] < prices[r]:
-                maxprofit = max(maxprofit, abs(prices[l] - prices[r]))
+                res = max(res, prices[r] - prices[l])
             else:
                 l = r
-            r += 1    
-        return maxprofit        
+            r += 1
+        return res        
+                   
