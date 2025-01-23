70df5cb48571c29f9b96904ff17a016b691792c6
Sam <51801821+samkim777@users.noreply.github.com>
2025-01-22 21:15:39 -0800

    Update file 1/22/2025, 9:15:39 PM

--- 121-BestTimetoBuyandSellStock/121-BestTimetoBuyandSellStock.py
+++ 121-BestTimetoBuyandSellStock/121-BestTimetoBuyandSellStock.py
@@ -1,11 +1,13 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        # [3,2,1,6,3,8]
         res = 0
-        l,r = 0,1
+        l,r = 0, 1
         while r < len(prices):
-            if prices[l] < prices[r]:
-                res = max(res, prices[r] - prices[l])
-            else:
+            if prices[l] >= prices[r]: # too big, increment
                 l = r
-            r += 1    
-        return res        
+            else: # calculate profit
+                res = max(res, prices[r] - prices[l])
+            r += 1
+
+        return res    
