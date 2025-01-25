bbfddb4f359b464a7b2c03fac55139b6ef04bb26
Sam <51801821+samkim777@users.noreply.github.com>
2025-01-24 22:31:46 -0800

    Create file

---
+++ 5-LongestPalindromicSubstring/5-LongestPalindromicSubstring.py
@@ -0,0 +1,24 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        # O(n^2) time complexity because we loop through the array twice
+        # O(1) space
+        if len(s) == 1:
+            return s
+        longest = s[0]
+        # At least two items
+        for n in range(len(s) - 1):
+            # even
+            l,r = n,n+1
+            while l >=0 and r < len(s) and s[l] == s[r]: # check for palindrome
+                if (r - l + 1) > len(longest):
+                    longest = s[l:r+1]
+                l -= 1
+                r += 1
+            # Odd
+            l, r = n,n
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                if (r - l + 1) > len(longest):
+                    longest = s[l:r+1]
+                l -= 1
+                r += 1        
+        return longest          
