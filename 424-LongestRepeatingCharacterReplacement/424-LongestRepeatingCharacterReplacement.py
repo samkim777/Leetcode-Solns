12111422e0cae591ebbe5c2adf5e24027c35de29
Sam <51801821+samkim777@users.noreply.github.com>
2025-01-29 22:11:20 -0800

    Update file 1/29/2025, 10:11:20 PM

--- 424-LongestRepeatingCharacterReplacement/424-LongestRepeatingCharacterReplacement.py
+++ 424-LongestRepeatingCharacterReplacement/424-LongestRepeatingCharacterReplacement.py
@@ -1,19 +1,19 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        l,r = 0,0
         count = {}
-        max_frequency = 0
-        max_length = 0
+        l = 0
+        res = 0
 
-        while r < len(s):
-            count[s[r]] = count.get(s[r], 0) + 1 # Get current count or init 0
-
-            max_frequency = max(max_frequency, count[s[r]])
-
-            if (r - l + 1 - max_frequency) > k: # No more replacements can be made
+        for r in range(len(s)):
+            count[s[r]] = 1 + count.get(s[r],0) # Increment current char count
+            
+            while (r - l + 1) - max(count.values()) > k: # More changes necces
                 count[s[l]] -= 1
                 l += 1
+            res = max(res,r - l + 1)
+        return res    
 
-            max_length = max(max_length, r - l + 1)
-            r += 1    
-        return max_length
+
+
+
+
