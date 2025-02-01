f89b4116195bf290a2027f3de2842aba15bff91d
Sam <51801821+samkim777@users.noreply.github.com>
2025-01-31 23:33:43 -0800

    Update file 1/31/2025, 11:33:43 PM

--- 567-PermutationinString/567-PermutationinString.py
+++ 567-PermutationinString/567-PermutationinString.py
@@ -1,37 +1,18 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        if len(s1) > len(s2): return False
-
-        s1Count, s2Count = [0] * 26, [0] * 26 #initialize hashmap length 26
+        if len(s1) > len(s2):
+            return False
+        
+        s1Count = [0] * 26
+        s2Count = [0] * 26
+        
         for i in range(len(s1)):
-            s1Count[ord(s1[i]) - ord('a')] += 1
-            s2Count[ord(s2[i]) - ord('a')] += 1 
+            s1Count[ord('a') - ord(s1[i])] += 1
+            s2Count[ord('a') - ord(s2[i])] += 1
 
-        matches = 0
-
-        for i in range(26):
-            matches += (1 if s1Count[i] == s2Count[i] else 0)
-
-        l = 0
-
-        for r in range(len(s1), len(s2)):
-            if matches == 26: return True
-            index = ord(s2[r]) - ord('a') # Key of right pointer 
-            s2Count[index] += 1 
-            if s1Count[index] == s2Count[index]:
-                matches += 1
-            elif s1Count[index] + 1 == s2Count[index]:
-                matches -= 1
-
-            index = ord(s2[l]) - ord('a') # Key of left pointer
-            s2Count[index] -= 1
-            if s1Count[index] == s2Count[index]:
-                matches += 1
-            elif s1Count[index] - 1 == s2Count[index]:
-                matches -= 1 
-            l += 1
-
-        return matches == 26                        
-
-
-                           
+        for k in range(len(s2) - len(s1)):
+            if s1Count == s2Count:
+                return True
+            s2Count[ord('a') - ord(s2[k])] -= 1
+            s2Count[ord('a') - ord(s2[k+len(s1)])] += 1
+        return s1Count == s2Count
