e48c98d680a71197711fdf35e51fcccfd88e1980
Sam <51801821+samkim777@users.noreply.github.com>
2025-02-02 16:24:03 -0800

    Update file 2/2/2025, 4:24:03 PM

--- 347-TopKFrequentElements/347-TopKFrequentElements.py
+++ 347-TopKFrequentElements/347-TopKFrequentElements.py
@@ -1,20 +1,16 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         hmap = {}
-        soln = [[] for _ in range(len(nums) + 1)]
-
+        heap = []
         for i in nums:
             if i not in hmap:
                 hmap[i] = 1
             else:
                 hmap[i] += 1
         
-        for num, count in hmap.items():
-            soln[count].append(num) 
-        
-        res = []
-        for i in range(len(soln) -1, -1,-1):
-            for num in soln[i]:
-                res.append(num)
-                if len(res) == k:
-                    return res
+        for num, freq in hmap.items():
+            if len(heap) < k:
+                heapq.heappush(heap, (freq,num))
+            else:
+                heapq.heappushpop(heap, (freq,num))
+        return [n[1] for n in heap]
