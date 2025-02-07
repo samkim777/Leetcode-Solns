80fdeba30255d6d0484f30fb22b52d03ed4b2615
Sam <51801821+samkim777@users.noreply.github.com>
2025-02-06 22:18:18 -0800

    Update file 2/6/2025, 10:18:17 PM

--- 22-GenerateParentheses/22-GenerateParentheses.py
+++ 22-GenerateParentheses/22-GenerateParentheses.py
@@ -10,12 +10,11 @@ class Solution:
             if openN == closedN == n:
                 res.append(sCur)
                 return
-
+            
             if openN < n:
                 backtrack(openN + 1, closedN, sCur+"(")
-
+            
             if closedN < openN:
-                backtrack(openN, closedN+1, sCur+")") 
-
+                backtrack(openN, closedN + 1, sCur+")")
         backtrack(0,0,"")
-        return res           
+        return res
