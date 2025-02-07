18a37037a9a9fcd024cc49fe74c17a52c8acf6d4
Sam <51801821+samkim777@users.noreply.github.com>
2025-02-06 22:20:27 -0800

    Update file 2/6/2025, 10:20:27 PM

--- 22-GenerateParentheses/22-GenerateParentheses.py
+++ 22-GenerateParentheses/22-GenerateParentheses.py
@@ -1,20 +1,21 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        # Conditions for backtracking:
-        # 1) # Open Brackets < n
-        # 2) # Closed Brackets < # Open Brackets
-        # 3) Valid IIF # Closed Brackets == # Opem Brackets
-        # Time: O(4n/sqrtn), Space: O(n)?
+        # condition for valid parenthese:
+        # open Brackets < closed Brackets
+        # closed brackets < n
+        # open brackets == closed brackets == n
         res = []
-        def backtrack(openN, closedN, sCur):
+
+        def backtrack(openN, closedN, cur):
             if openN == closedN == n:
-                res.append(sCur)
+                res.append(cur)
                 return
             
             if openN < n:
-                backtrack(openN + 1, closedN, sCur+"(")
-            
+                backtrack(openN + 1, closedN, cur+"(")
+
             if closedN < openN:
-                backtrack(openN, closedN + 1, sCur+")")
+                backtrack(openN, closedN + 1, cur+")")
+        
         backtrack(0,0,"")
         return res
