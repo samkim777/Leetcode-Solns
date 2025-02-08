b3ae290ecf16b8031b47449a301f662019bad1d4
Sam <51801821+samkim777@users.noreply.github.com>
2025-02-07 22:07:20 -0800

    Update file 2/7/2025, 10:07:20 PM

--- 9-PalindromeNumber/9-PalindromeNumber.py
+++ 9-PalindromeNumber/9-PalindromeNumber.py
@@ -2,9 +2,7 @@ class Solution:
     def isPalindrome(self, x: int) -> bool:
         back_x = 0
         orig = x
-
         while x > 0:
-            back_x = (back_x * 10) + (x % 10) # last digit + 10 * last, 
-            # so it'll be like 5, 54, 543, 5432, 54321 for 12345
+            back_x = back_x * 10 + x % 10
             x = x // 10
-        return orig == back_x
+        return back_x == orig
