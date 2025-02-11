3a5dc4b36b057d79970dc1a2327fc58ee33de6b1
Sam <51801821+samkim777@users.noreply.github.com>
2025-02-10 21:54:14 -0800

    File updated at 2/10/2025, 9:54:13 PM

--- 9-PalindromeNumber/9-PalindromeNumber.py
+++ 9-PalindromeNumber/9-PalindromeNumber.py
@@ -1,7 +1,9 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
+        # crackhead solution ?XD
         back_x = 0
         orig = x
+
         while x > 0:
             back_x = back_x * 10 + x % 10
             x = x // 10
