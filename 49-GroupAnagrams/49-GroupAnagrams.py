e0ef5381f5bf35b084ef2eba48fbd44407fad674
Sam <51801821+samkim777@users.noreply.github.com>
2025-02-15 21:51:48 -0800

    File updated at 2/15/2025, 9:51:48 PM

--- 49-GroupAnagrams/49-GroupAnagrams.py
+++ 49-GroupAnagrams/49-GroupAnagrams.py
@@ -1,5 +1,9 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # Create a dictionary where the key is array of ascii values
+        # Append all strings with the same array into that key
+        # have the value for each key be an array itself
+        # ** Remember that keys cannot be mutable in python
         group = defaultdict(list)
         for s in strs:
             chars = [0] * 26
