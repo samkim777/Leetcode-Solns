af252b2dfd262ead386c912807827fe5ad72e7e1
Sam <51801821+samkim777@users.noreply.github.com>
2025-02-18 22:16:37 -0800

    File updated at 2/18/2025, 10:16:37 PM

--- 21-MergeTwoSortedLists/21-MergeTwoSortedLists.py
+++ 21-MergeTwoSortedLists/21-MergeTwoSortedLists.py
@@ -5,26 +5,22 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        # Create a dummy linked list
         dummy = ListNode()
-
-        # In order to maintain pointer in the correct position for dummy, create another pointer
         tail = dummy
 
-        # At each node, compare and set the next of dummy accordingly
         while list1 and list2:
-            if list1.val > list2.val:
-                tail.next = list2
-                list2 = list2.next
-            else:
+            if list1.val < list2.val:
                 tail.next = list1
                 list1 = list1.next
+            else:
+                tail.next = list2
+                list2 = list2.next
             tail = tail.next
         
-        # If any of the lists are non-empty, attach remaining nodes to dummy
-        if list1:
+        if list1: # list1 remains, attach to rest of dummy
             tail.next = list1
-        else:
+        
+        if list2:
             tail.next = list2
         
         return dummy.next
