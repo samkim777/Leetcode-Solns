8f54079a358b23647936074a057663faebf16166
Sam <51801821+samkim777@users.noreply.github.com>
2025-02-26 21:54:25 -0800

    File updated at 2/26/2025, 9:54:24 PM

--- 21-MergeTwoSortedLists/21-MergeTwoSortedLists.py
+++ 21-MergeTwoSortedLists/21-MergeTwoSortedLists.py
@@ -6,20 +6,19 @@
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
         dummy = ListNode()
-        cur = dummy
-        
+        tail = dummy
+        # Create dummy
         while list1 and list2:
             if list1.val > list2.val:
-                cur.next = list2
+                tail.next = list2
                 list2 = list2.next
             else:
-                cur.next = list1
+                tail.next = list1
                 list1 = list1.next
-            cur = cur.next
-        
+            tail = tail.next
         if list1:
-            cur.next = list1
+            tail.next = list1
         else:
-            cur.next = list2
+            tail.next = list2
         
         return dummy.next
