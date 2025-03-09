9e6ff6e33a6eed1e2ece2567cf76e36f2e97f7ce
Sam <51801821+samkim777@users.noreply.github.com>
2025-03-08 22:45:02 -0800

    File updated at 3/8/2025, 10:45:02 PM

--- 1448-CountGoodNodesinBinaryTree/1448-CountGoodNodesinBinaryTree.py
+++ 1448-CountGoodNodesinBinaryTree/1448-CountGoodNodesinBinaryTree.py
@@ -1,3 +1,4 @@
+// Last updated: 3/8/2025, 10:45:02 PM
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -6,14 +7,25 @@
 #         self.right = right
 class Solution:
     def goodNodes(self, root: TreeNode) -> int:
-        def dfs(root, max_sofar):
+        # Determine return value and state
+        # Return value: 0 when we hit base case
+        # Return value cannot be boolean because we're trying to return
+        # number of good nodes, so boolean doesn't make sense
+        # State? maximum value seen so far
+        def dfs(root, max_val):
             if not root:
                 return 0
-            total = 0
-            if root.val >= max_sofar:
-                max_sofar = root.val
-                total += 1
-            total += dfs(root.left, max_sofar)
-            total += dfs(root.right, max_sofar)
-            return total
+            # Declare total var, this will be returned to keep track of number
+            # of good nodes
+            goods = 0
+            # is the current node value greater than or equal to max seen so far?
+            if root.val >= max_val:
+                goods += 1
+                max_val = root.val
+            
+            # Traverse left and right nodes
+            goods += dfs(root.left, max_val)
+            goods += dfs(root.right, max_val)
+            # Return this variable to keep track of number of good nodes
+            return goods
         return dfs(root, float('-inf'))
