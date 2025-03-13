fda151fcf95fecc1c31855ef6d28d76377a8a2eb
Sam <51801821+samkim777@users.noreply.github.com>
2025-03-12 21:57:14 -0700

    File updated at 3/12/2025, 9:57:14 PM

--- 105-ConstructBinaryTreefromPreorderandInorderTraversal/105-ConstructBinaryTreefromPreorderandInorderTraversal.py
+++ 105-ConstructBinaryTreefromPreorderandInorderTraversal/105-ConstructBinaryTreefromPreorderandInorderTraversal.py
@@ -1,3 +1,4 @@
+// Last updated: 3/12/2025, 9:57:14 PM
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -6,14 +7,21 @@
 #         self.right = right
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
-        # Preorder is M -> L -> R
-        # Inorder is L -> M -> R
-        # First element of preoder is always node
+        # preorder is middle -> Left -> RIght
+        # inorder is left -> middle - Right
+        # The first element of preorder is always the root
+        # the elements to the left of preorder's middle found inside
+        # inorder are the left subtree, and right is right subtree
+        # Recursively go through both trees to construct binary tree
+        # time complexity of O(n), space O(n) [Recurse calls]
+
+        # base case
         if not preorder or not inorder:
             return None
         tree = TreeNode(preorder[0])
-        mid = inorder.index(preorder[0]) # Find node inside inorder
-        # Recurse
-        tree.left = self.buildTree(preorder[1:mid + 1], inorder[:mid])
-        tree.right = self.buildTree(preorder[mid + 1:], inorder[mid+1:])
+        parent_index = inorder.index(preorder[0])
+
+        tree.left = self.buildTree(preorder[1:parent_index + 1], inorder[:parent_index + 1])
+        tree.right = self.buildTree(preorder[parent_index+1:], inorder[parent_index + 1:])
+
         return tree
