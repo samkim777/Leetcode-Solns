59031c0b09a93627033a4967942e93709279ace0
Sam <51801821+samkim777@users.noreply.github.com>
2025-03-13 22:20:57 -0700

    File updated at 3/13/2025, 10:20:57 PM

--- 226-InvertBinaryTree/226-InvertBinaryTree.py
+++ 226-InvertBinaryTree/226-InvertBinaryTree.py
@@ -1,4 +1,4 @@
-// Last updated: 3/11/2025, 10:39:04 PM
+// Last updated: 3/13/2025, 10:20:57 PM
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -7,15 +7,14 @@
 #         self.right = right
 class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        # No state or value to return
         def dfs(root):
-            # Base case
             if not root:
-                return None
-            # swap left and right
-            dfs(root.right)
-            dfs(root.left)
-            temporary = root.right
+                return
+            temp = root.right
             root.right = root.left
-            root.left = temporary
+            root.left = temp
+            dfs(root.left)
+            dfs(root.right)
         dfs(root)
         return root
