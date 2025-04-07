f0575afda0c088b129df615298157661dd05f5e0
Sam <51801821+samkim777@users.noreply.github.com>
2025-04-06 23:20:13 -0700

    Create file

---
+++ 404-SumofLeftLeaves/404-SumofLeftLeaves.py
@@ -0,0 +1,20 @@
+# Last updated: 4/6/2025, 11:20:13 PM
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sumOfLeftLeaves(self, root: Optional[TreeNode]) -> int:
+        # Weird problem, but I think similar to the 
+        # right view of tree, where we traverse each level 
+        # to get the right most tree only
+        lsum = 0
+        def dfs(root, isLeft):
+            if not root:
+                return 0
+            if not root.left and not root.right:
+                return root.val if isLeft else 0
+            return dfs(root.left, True) + dfs(root.right, False)
+        return dfs(root, False)
