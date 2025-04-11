f0ca4ba23bc0efe0598cf2d128564e8134198709
Sam <51801821+samkim777@users.noreply.github.com>
2025-04-10 22:33:35 -0700

    File updated at 4/10/2025, 10:33:34 PM

--- 102-BinaryTreeLevelOrderTraversal/102-BinaryTreeLevelOrderTraversal.py
+++ 102-BinaryTreeLevelOrderTraversal/102-BinaryTreeLevelOrderTraversal.py
@@ -1,4 +1,4 @@
-# Last updated: 4/5/2025, 10:04:33 PM
+# Last updated: 4/10/2025, 10:33:34 PM
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -7,17 +7,15 @@
 #         self.right = right
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        # The trick to this problem is to use the idea of levels
-        # while utilizing dfs. We also note the fact that we could
-        # just as easily use bfs to implement the solution as well
+        # Each "level" needs to be a separate array
         res = []
-        def dfs(root,level):
+        def dfs(root, level):
             if not root:
-                return
+                return None
             if len(res) == level:
                 res.append([])
             res[level].append(root.val)
-            dfs(root.left,level + 1)
+            dfs(root.left, level + 1)
             dfs(root.right, level + 1)
         dfs(root,0)
         return res
