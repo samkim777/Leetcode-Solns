1e673320c1f6c97f9a891d5573a6890bd1bfe3d1
Sam <51801821+samkim777@users.noreply.github.com>
2025-04-11 22:34:01 -0700

    File updated at 4/11/2025, 10:34:01 PM

--- 98-ValidateBinarySearchTree/98-ValidateBinarySearchTree.py
+++ 98-ValidateBinarySearchTree/98-ValidateBinarySearchTree.py
@@ -1,4 +1,4 @@
-# Last updated: 4/11/2025, 10:32:51 PM
+# Last updated: 4/11/2025, 10:34:01 PM
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -7,19 +7,13 @@
 #         self.right = right
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        # Binary Search tree contains no duplicates
-        # Left subtree must all have values less than current node
-        # Right subtree must all have values greater than current node
-        # Use DFS to recursively check nodes
-        # Determine base case: Return value: if reached end of tree, return True
-        # because returning false will be tricky to handle as it might mean the tree is not valid
-        # States: the current node's left and right children must be less and greater 
-        # respectively, so should keep their values
-        # in this case easier to just define dfs function to handle states
-        def dfs(root, left_val, right_val):
+        # need to keep LEFT and RIGHT values for all nodes
+        # parent needs to be SMALLER THAN ALL LEFT
+        # parent needs to be GREATER THAN ALL RIGHT
+        def dfs(root, l, r):
             if not root:
                 return True
-            if not (left_val < root.val < right_val):
+            if not (l < root.val < r):
                 return False
-            return dfs(root.left, left_val, root.val) and dfs(root.right, root.val, right_val)
+            return dfs(root.left, l,root.val) and dfs(root.right,root.val,r)
         return dfs(root, float("-inf"), float("inf"))
