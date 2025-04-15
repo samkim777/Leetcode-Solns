a9a25753b4cd290db7e2223c6ca98453345063d9
Sam <51801821+samkim777@users.noreply.github.com>
2025-04-14 22:56:22 -0700

    File updated at 4/14/2025, 10:56:22 PM

--- 78-Subsets/78-Subsets.py
+++ 78-Subsets/78-Subsets.py
@@ -1,22 +1,17 @@
-# Last updated: 4/14/2025, 10:40:21 PM
+# Last updated: 4/14/2025, 10:56:22 PM
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         res = []
-        path = []
-        # Backtracking
-        # Contraint: Current list of element is inside the results already
-        # Goal: Return all possible subsets of list of integers
-        # Ex: Given [1,2,3]: return [], [1],[2],[3],[1,2],[1,3],[2,1],[2,3],[3,1]... etc. Then filter out any duplicates
-        def backtrack(index):
-            if index >= len(nums):
-                res.append(path.copy())
-                return
-            # Decision to add the cur number to list
-            path.append(nums[index])
-            backtrack(index + 1)
+        subset = []
+        def backtrack(i):
+            # base condition
+            if i >= len(nums):
+                res.append(subset.copy())
+                return  
+            subset.append(nums[i])
+            backtrack(i+1)
 
-            # Decision to not add the cur number to list
-            path.pop()
-            backtrack(index + 1)    
+            subset.pop()
+            backtrack(i+1)
         backtrack(0)
-        return res              
+        return res
