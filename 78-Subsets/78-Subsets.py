a7b79569b555d027899d3e3a23452dc55d3b5114
Sam <51801821+samkim777@users.noreply.github.com>
2025-04-15 22:53:12 -0700

    File updated at 4/15/2025, 10:53:11 PM

--- 78-Subsets/78-Subsets.py
+++ 78-Subsets/78-Subsets.py
@@ -1,4 +1,4 @@
-# Last updated: 4/14/2025, 11:08:39 PM
+# Last updated: 4/15/2025, 10:53:11 PM
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         res = []
@@ -8,11 +8,10 @@ class Solution:
             if i >= len(nums):
                 res.append(subset.copy())
                 return
-            
             subset.append(nums[i])
-            backtrack(i+1)
+            backtrack(i+1) # Continue on
 
-            subset.pop()
+            subset.pop() # [1,2,3] -> [1,2] Consider options where we don't include
             backtrack(i+1)
         backtrack(0)
         return res
