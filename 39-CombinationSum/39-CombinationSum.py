8af60298a1eff237a0f33469325309b0ec2e56a8
Sam <51801821+samkim777@users.noreply.github.com>
2025-04-17 22:45:07 -0700

    File updated at 4/17/2025, 10:45:07 PM

--- 39-CombinationSum/39-CombinationSum.py
+++ 39-CombinationSum/39-CombinationSum.py
@@ -1,30 +1,19 @@
-# Last updated: 4/16/2025, 10:07:04 PM
+# Last updated: 4/17/2025, 10:45:07 PM
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        # distinct integers candidates
-        # target int
-        # return LIST of all UNIQUE COMBINATIONS OF CANDIDATES 
-        # WHERE sum equals TARGET
-        # [2,3,6,7], target = 7
-        # [[2,2,3], [7]]
-        # but not [2,3,2], [3,2,2] etc
         res = []
-        cur = []
-        def backtrack(i, cur,curSum):
-            # Base case
+        def backtrack(i,cur,curSum):
+            # base case
             if curSum == target:
                 res.append(cur.copy())
                 return
-            if i >= len(candidates) or target < curSum:
-                return
-            
-            cur.append(candidates[i])
 
-            backtrack(i, cur,curSum + candidates[i])
+            if i >= len(candidates) or curSum > target:
+                return
+            cur.append(candidates[i])
+            backtrack(i,cur,curSum + candidates[i])
 
             cur.pop()
-            backtrack(i + 1, cur, curSum)
+            backtrack(i + 1,cur, curSum)
         backtrack(0,[],0)
         return res
-            
-            
