54c341ac0fde5f9a93678034dee4383d1a0cde9b
Sam <51801821+samkim777@users.noreply.github.com>
2025-04-18 21:57:55 -0700

    File updated at 4/18/2025, 9:57:55 PM

--- 78-Subsets/78-Subsets.py
+++ 78-Subsets/78-Subsets.py
@@ -1,17 +1,16 @@
-# Last updated: 4/15/2025, 10:53:11 PM
+# Last updated: 4/18/2025, 9:57:55 PM
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         res = []
-        subset = []
-        def backtrack(i):
+        def backtrack(i, subset):
             # base case
             if i >= len(nums):
                 res.append(subset.copy())
                 return
             subset.append(nums[i])
-            backtrack(i+1) # Continue on
+            backtrack(i+1, subset) # Continue on
 
             subset.pop() # [1,2,3] -> [1,2] Consider options where we don't include
-            backtrack(i+1)
-        backtrack(0)
+            backtrack(i+1, subset)
+        backtrack(0, [])
         return res
