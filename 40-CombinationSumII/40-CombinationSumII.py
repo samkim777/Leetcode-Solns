3c9f8b8bd83b7f4ccbbf2a2d3c190498972c97a4
Sam <51801821+samkim777@users.noreply.github.com>
2025-04-20 22:30:43 -0700

    File updated at 4/20/2025, 10:30:42 PM

--- 40-CombinationSumII/40-CombinationSumII.py
+++ 40-CombinationSumII/40-CombinationSumII.py
@@ -1,29 +1,26 @@
-# Last updated: 4/19/2025, 5:05:23 PM
+# Last updated: 4/20/2025, 10:30:42 PM
 class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
-        candidates.sort() # skip duplicates
+        # riiiight you have to sort
+        candidates.sort()
         res = []
         cur = []
         def backtrack(i,curSum):
+            # base cases
             if target == curSum:
                 res.append(cur.copy())
                 return
-            if target < curSum or i >= len(candidates):
+            if i >= len(candidates) or target < curSum:
                 return
 
             cur.append(candidates[i])
             backtrack(i+1, curSum + candidates[i])
 
+            # No duplicates are allowed
             cur.pop()
-            # When we backtrack, we should not include the same number again
-            # in our decision tree because otherwise we will include
-            # duplicate combination sums
-            # ie, [1,1,5] and [1,5,1]
-            # So the way to skip it is to check if the next val duplicate
-            # and keep incrementing pointer if so
-            next_i = i + 1
-            while next_i < len(candidates) and candidates[next_i] == candidates[i]:
-                next_i += 1
-            backtrack(next_i, curSum)
+            backIndex = i + 1
+            while backIndex < len(candidates) and candidates[i] == candidates[backIndex]:
+                backIndex += 1
+            backtrack(backIndex ,curSum)
         backtrack(0,0)
         return res
