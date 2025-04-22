53bf2a4d83776e7300e587d866dcd4bd6bf37ed8
Sam <51801821+samkim777@users.noreply.github.com>
2025-04-21 22:12:22 -0700

    File updated at 4/21/2025, 10:12:21 PM

--- 104-MaximumDepthofBinaryTree/104-MaximumDepthofBinaryTree.py
+++ 104-MaximumDepthofBinaryTree/104-MaximumDepthofBinaryTree.py
@@ -1,3 +1,4 @@
+# Last updated: 4/21/2025, 10:12:21 PM
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -6,10 +7,16 @@
 #         self.right = right
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
+        # if we use dfs,and compare the values returned from
+        # recursing the left and right subtree, and take the maximum,
+        # we should get the maximum depth of our tree
+        # O(n) time complexity, O(h) avg space, O(n) worst space
+        # if the tree looks a lil funky and is like a line
         def dfs(root):
+            # base case, if none,just return 0
             if not root:
                 return 0
-            left = dfs(root.left)
-            right = dfs(root.right)
-            return max(left,right) + 1
+            left=dfs(root.left)
+            right=dfs(root.right)
+            return 1 + max(left,right)
         return dfs(root)
