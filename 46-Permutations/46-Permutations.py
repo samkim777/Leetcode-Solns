40ceaaf2f7afb0cbc0497b0fc07eec84c51203d3
Sam <51801821+samkim777@users.noreply.github.com>
2025-04-30 20:53:28 -0700

    File updated at 4/30/2025, 8:53:27 PM

--- 46-Permutations/46-Permutations.py
+++ 46-Permutations/46-Permutations.py
@@ -1,33 +1,19 @@
-# Last updated: 4/29/2025, 11:52:09 PM
+# Last updated: 4/30/2025, 8:53:27 PM
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        # Given array of distinct integers return all possible permutations
-        # [1,2,3] --> [1,2,3], [1,3,2]...
-        # In essence, [1,2,3], then [1,2] then [1] then [1,3]...
-        # backtrack([])
-        # backtrack([1])
-        # backtrack([1,2])
-        # backtrack([1,2])
-        # backtrack([1,2,3]) # return and save
-        # backtrack([1,2])
-        # backtrack([1]) # loop index at 3 so we do that
-        # backtrack([1,3]) # recurse
-        # backtrack([1,3,2])
-        # backtrack([1,3])
-        # backtrack([1])
-        # backtrack([])
-        # backtrack([2])... cont
         res = []
         def backtrack(cur):
-            # base case, length of cur == length of nums
+            # base case
             if len(nums) == len(cur):
-                res.append(cur.copy()) # shallow copy to avoid issues while recursing
+                res.append(cur.copy())
                 return
             for num in nums:
+                # If already seen skip
                 if num in cur:
                     continue
-                cur.append(num) # [1], [1,2], [1,2,3]
+                cur.append(num)
                 backtrack(cur)
+                # Pop and search other permutations
                 cur.pop()
         backtrack([])
         return res
