5d7d2de37f55e90ffa8b24332d7b3b1beca6d5a6
Sam <51801821+samkim777@users.noreply.github.com>
2025-05-01 23:29:19 -0700

    File updated at 5/1/2025, 11:29:19 PM

--- 47-PermutationsII/47-PermutationsII.py
+++ 47-PermutationsII/47-PermutationsII.py
@@ -1,24 +1,25 @@
-# Last updated: 5/1/2025, 11:25:50 PM
+# Last updated: 5/1/2025, 11:29:19 PM
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
         res = []
         nums.sort()
         used = [False] * len(nums)
-        def backtrack(cur):
+
+        def baktrack(cur):
             if len(cur) == len(nums):
                 res.append(cur.copy())
                 return
-            
             for i in range(len(nums)):
                 if used[i]:
                     continue
                 if i > 0 and nums[i] == nums[i-1] and not used[i-1]:
                     continue
+                
                 used[i] = True
                 cur.append(nums[i])
-                backtrack(cur)
+                baktrack(cur)
 
                 cur.pop()
                 used[i] = False
-        backtrack([])
+        baktrack([])
         return res
