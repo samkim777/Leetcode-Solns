b7cfb47a8c18265c9cce0139b3bc1430c8fcf0a7
Sam <51801821+samkim777@users.noreply.github.com>
2025-05-03 23:08:25 -0700

    File updated at 5/3/2025, 11:08:25 PM

--- 39-CombinationSum/39-CombinationSum.py
+++ 39-CombinationSum/39-CombinationSum.py
@@ -1,19 +1,22 @@
-# Last updated: 4/28/2025, 9:42:27 PM
+# Last updated: 5/3/2025, 11:08:25 PM
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        # only unique combinations
+        # elements inside candidates is unique, so no need to worry about
+        # duplicates
         res = []
-        def backtrack(i,cur,curSum):
+        def backtrack(i, cur, curSum):
+            # base case
             if target == curSum:
-                res.append(cur.copy()) # shallow copy to not mess with cur array being passed in
+                res.append(cur.copy())
                 return
-            if i >= len(candidates) or curSum > target:
+            if i >= len(candidates) or target < curSum:
                 return
-            
-            # choose to add
+            # include
             cur.append(candidates[i])
-            backtrack(i, cur,curSum + candidates[i])
-
-            # Remove and backtrack
+            backtrack(i, cur, curSum + candidates[i])
+            
+            # not include
             cur.pop()
             backtrack(i+1, cur, curSum)
         backtrack(0,[],0)
