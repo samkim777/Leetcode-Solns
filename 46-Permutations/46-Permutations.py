7e23c2a1b291b345ebd6435bb92b48a095057b74
Sam <51801821+samkim777@users.noreply.github.com>
2025-05-08 22:41:39 -0700

    File updated at 5/8/2025, 10:41:39 PM

--- 46-Permutations/46-Permutations.py
+++ 46-Permutations/46-Permutations.py
@@ -1,23 +1,22 @@
-# Last updated: 5/1/2025, 11:12:00 PM
+# Last updated: 5/8/2025, 10:41:39 PM
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        # In a decision tree,
-        # Add 1, Then add 2 or 3, Then add 3 or 2
-        # Add 2 Then add 1 or 3, then add 3 or 1
-        # Add 3 Then add 1 or 2, then add 2 or 1
-        # Use a for loop to loop over every number
-        # Since permutation, O(n!) time, space is O(n)?
+        # permutations, order doesn't matter
+        # no duplicates
         res = []
-        def backtrack(cur):
-            if len(cur) == len(nums):
+        def backtrack(i,cur):
+            if len(nums) == len(cur):
                 res.append(cur.copy())
+                return
+            if i >= len(nums):
                 return
             for n in nums:
                 if n in cur:
                     continue
                 cur.append(n)
-                backtrack(cur)
+                backtrack(i+1,cur)
 
-                cur.pop() # Try other permutations
-        backtrack([])
+                cur.pop()
+                backtrack(i+1, cur)
+        backtrack(0,[])
         return res
