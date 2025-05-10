e55a63e294889aee8c3d1b284781adec301b20e7
Sam <51801821+samkim777@users.noreply.github.com>
2025-05-09 23:03:03 -0700

    File updated at 5/9/2025, 11:03:03 PM

--- 90-SubsetsII/90-SubsetsII.py
+++ 90-SubsetsII/90-SubsetsII.py
@@ -1,23 +1,18 @@
-# Last updated: 5/6/2025, 10:05:47 PM
+# Last updated: 5/9/2025, 11:03:03 PM
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
-        # for subsets, since the order is linear, we can use
-        # while loop to skip index instead of for loop ??
         res = []
-        nums.sort() # have to sort for easier dup check
+        nums.sort()
         def backtrack(i,cur):
-            # base case
             if i >= len(nums):
                 res.append(cur.copy())
                 return
             
-            # Decision to add
             cur.append(nums[i])
             backtrack(i+1, cur)
 
             cur.pop()
-            # skip dupliactes
-            while i < len(nums) - 1 and nums[i] == nums[i + 1]:
+            while i < len(nums) - 1 and nums[i] == nums[i+1]:
                 i += 1
             backtrack(i+1, cur)
         backtrack(0,[])
