3f39b94c34a476e966fd21aae93681e3fe2466c7
Sam <51801821+samkim777@users.noreply.github.com>
2025-05-11 22:49:38 -0700

    File updated at 5/11/2025, 10:49:38 PM

--- 40-CombinationSumII/40-CombinationSumII.py
+++ 40-CombinationSumII/40-CombinationSumII.py
@@ -1,21 +1,30 @@
-# Last updated: 5/10/2025, 10:55:08 PM
+# Last updated: 5/11/2025, 10:49:38 PM
 class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
         candidates.sort()
         res = []
-        def backtrack(i, cur, curVal):
-            if target == curVal:
-                res.append(cur.copy())
-                return
-            if target < curVal or i >= len(candidates):
-                return
-            cur.append(candidates[i])
-            backtrack(i + 1,cur, curVal + candidates[i])
 
-            cur.pop()
-            index = i + 1
-            while index < len(candidates) and candidates[i] == candidates[index]:
-                index += 1
-            backtrack(index, cur, curVal)
-        backtrack(0,[],0)
+        def backtrack(i,curVal, curArr):
+            # base case: Found target
+            if target == curVal:
+                res.append(curArr.copy()) # We add a shallow copy
+                # since it is being passed through recursion
+                return
+            # target too small with current sum
+            # index out of array
+            if i >= len(candidates) or target < curVal:
+                return
+            # Decision to add
+            curArr.append(candidates[i])
+            backtrack(i+1, curVal + candidates[i], curArr)
+
+            # Decision to not add
+            curArr.pop()
+            # Skip duplicate
+            while i < len(candidates) - 1 and candidates[i] == candidates[i+1]:
+                i+=1
+            backtrack(i+1, curVal, curArr)
+            # we don't need to add curVal with another value because it'll 
+            # recurse up anyways to handle when we follow "add" decision
+        backtrack(0,0,[])
         return res
