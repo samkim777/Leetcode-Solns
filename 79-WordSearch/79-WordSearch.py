8af820a43823eafabf526e2280e7b7393ff5f9f3
Sam <51801821+samkim777@users.noreply.github.com>
2025-05-12 23:45:55 -0700

    File updated at 5/12/2025, 11:45:55 PM

--- 79-WordSearch/79-WordSearch.py
+++ 79-WordSearch/79-WordSearch.py
@@ -1,23 +1,33 @@
+# Last updated: 5/12/2025, 11:45:55 PM
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        # Utilize dfs?
-        row = len(board)
-        col = len(board[0])
-        visited = set()
-
-        def dfs(r,c,i):
-            if i == len(word):
+        # Edge case: word length > board length
+        # won't have to worry about floats, ints, special characters, etc
+        # dfs approach to search sequential cells
+        # if we reach index length of word, then True
+        # else False
+        # Recursive steps: Search top, left, right, bottom 
+        # if current character is same as word's character at index
+        # add to our cur string and continue else skip
+        Result = False
+        row,col = len(board), len(board[0])
+        def dfs(r,c, index):
+            if index == len(word):
                 return True
-
-            if r < 0 or c < 0 or r >= row or c >= col or (r,c) in visited or word[i] != board[r][c]:
+            if r < 0 or r >= row or c < 0 or c >= col or board[r][c] != word[index]:
                 return False
-            visited.add((r,c))
-            res = dfs(r+1,c, i+1) or dfs(r-1,c, i+1) or dfs(r,c-1, i+1) or dfs(r,c+1, i+1)
-            visited.remove((r,c))
+            
+            tmp = board[r][c]
+            board[r][c] = "#"
+            res = dfs(r+1,c,index+1) or dfs(r-1, c, index+1) or dfs(r, c+1, index+1) or dfs(r,c-1, index+1)
+
+            board[r][c] = tmp
             return res
 
+        if len(word) > row*col:
+            return False
         for r in range(row):
             for c in range(col):
                 if dfs(r,c,0):
                     return True
-        return False              
+        return False
