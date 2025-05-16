4f52e6646c2b6017f3c36a850fef0db51b0ab46b
Sam <51801821+samkim777@users.noreply.github.com>
2025-05-15 23:32:27 -0700

    File updated at 5/15/2025, 11:32:27 PM

--- 79-WordSearch/79-WordSearch.py
+++ 79-WordSearch/79-WordSearch.py
@@ -1,29 +1,24 @@
-# Last updated: 5/13/2025, 11:26:40 PM
+# Last updated: 5/15/2025, 11:32:27 PM
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        # matrix traversal using dfs
-        # if the current grid character is inside our word
-        # then we will dfs from there after having marked it as visited
-        # otherwise, skip
-        # Time complexity O(m*n*4^n) where n is length of word
-        # space complexity: O(m*n)
         rows,cols = len(board), len(board[0])
+        if len(word) > rows * cols:
+            return False
+
         def dfs(r,c,i):
-            # base cases
+            # base case
             if i == len(word):
                 return True
             if r < 0 or r >= rows or c < 0 or c >= cols or word[i] != board[r][c]:
                 return False
-            # Visited and inside word
+            # To keep track of the grid that we visited, we mark it as X
             tmp = board[r][c]
-            board[r][c] = "#"
-            # Now we'll have to search the four directions
-            res = dfs(r+1,c,i+1) or dfs(r-1,c,i+1) or dfs(r, c+1, i+1) or dfs(r,c-1,i+1)
+            board[r][c] = "X"
+
+            cur = dfs(r+1,c,i+1) or dfs(r-1,c,i+1) or dfs(r,c+1,i+1) or dfs(r,c-1,i+1)
             board[r][c] = tmp
-            return res
+            return cur
         
-        if len(word) > rows * cols:
-            return False
         for r in range(rows):
             for c in range(cols):
                 if dfs(r,c,0):
