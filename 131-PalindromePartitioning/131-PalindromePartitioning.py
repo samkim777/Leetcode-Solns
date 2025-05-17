c427567b2083fb0a25cb0157b20944d3a5651cab
Sam <51801821+samkim777@users.noreply.github.com>
2025-05-16 22:38:42 -0700

    File updated at 5/16/2025, 10:38:42 PM

--- 131-PalindromePartitioning/131-PalindromePartitioning.py
+++ 131-PalindromePartitioning/131-PalindromePartitioning.py
@@ -1,18 +1,8 @@
-# Last updated: 5/16/2025, 10:36:16 PM
+# Last updated: 5/16/2025, 10:38:42 PM
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
-        # Partition a string s such that every substring of the partition
-        # is a palindrome
-        # ex) "aab" --> [["a","a","b"], ["aa","b"]]
-        # the character itself is a palindrome
-        # s = "abc"
-        # [["a" "b" "c"]]
-        # s = "aba"
-        # [["a" "b" "a"], ["aba"]]
         res = []
-
         def isPalindrome(c):
-            # logic for checking if palindrome
             l,r = 0, len(c) - 1
             while l <= r:
                 if c[l] != c[r]:
@@ -20,18 +10,15 @@ class Solution:
                 l += 1
                 r -= 1
             return True
-
+            
         def backtrack(i,curArr):
             if i >= len(s):
                 res.append(curArr.copy())
                 return
-
-            for j in range(i,len(s)):
-                if isPalindrome(s[i:j+1]):
-                    curArr.append(s[i:j+1])
-                    backtrack(j+1, curArr)
+            for k in range(i,len(s)):
+                if isPalindrome(s[i:k+1]):
+                    curArr.append(s[i:k+1])
+                    backtrack(k+1,curArr)
                     curArr.pop()
         backtrack(0,[])
         return res
-                
-        
