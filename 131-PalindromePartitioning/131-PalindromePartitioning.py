7bff6aa9c36c259005fdccbd9d94a737127992a5
Sam <51801821+samkim777@users.noreply.github.com>
2025-05-18 23:46:44 -0700

    File updated at 5/18/2025, 11:46:44 PM

--- 131-PalindromePartitioning/131-PalindromePartitioning.py
+++ 131-PalindromePartitioning/131-PalindromePartitioning.py
@@ -1,24 +1,27 @@
-# Last updated: 5/16/2025, 10:38:42 PM
+# Last updated: 5/18/2025, 11:46:44 PM
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
+        # Objective: Given string, return subsets of the string
+        # such that each subset contains characters that are palindromes
+        # aab --> [a a b] [aa b]
+        # Time: O(n*2^n), space: O(n)
         res = []
-        def isPalindrome(c):
-            l,r = 0, len(c) - 1
+        def isPalindrome(char):
+            l,r = 0, len(char) - 1
             while l <= r:
-                if c[l] != c[r]:
+                if char[l] != char[r]:
                     return False
                 l += 1
                 r -= 1
             return True
-            
-        def backtrack(i,curArr):
+        def backtrack(i,cur):
             if i >= len(s):
-                res.append(curArr.copy())
+                res.append(cur.copy())
                 return
-            for k in range(i,len(s)):
-                if isPalindrome(s[i:k+1]):
-                    curArr.append(s[i:k+1])
-                    backtrack(k+1,curArr)
-                    curArr.pop()
+            for c in range(i,len(s)):
+                if isPalindrome(s[i:c+1]): # 
+                    cur.append(s[i:c+1])
+                    backtrack(c+1, cur)
+                    cur.pop()
         backtrack(0,[])
         return res
