2a1eb651588e3b50d16f8b737794097054d199d8
Sam <51801821+samkim777@users.noreply.github.com>
2025-05-19 23:11:56 -0700

    File updated at 5/19/2025, 11:11:56 PM

--- 79-WordSearch/79-WordSearch.py
+++ 79-WordSearch/79-WordSearch.py
@@ -1,26 +1,22 @@
-# Last updated: 5/15/2025, 11:32:27 PM
+# Last updated: 5/19/2025, 11:11:56 PM
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        rows,cols = len(board), len(board[0])
-        if len(word) > rows * cols:
-            return False
-
-        def dfs(r,c,i):
-            # base case
+        rows, cols = len(board), len(board[0])
+        def backtrack(r,c,i):
+            # base case when we find the char
             if i == len(word):
                 return True
+            # base case for when if out of bounds or not correct char
             if r < 0 or r >= rows or c < 0 or c >= cols or word[i] != board[r][c]:
                 return False
-            # To keep track of the grid that we visited, we mark it as X
-            tmp = board[r][c]
+            # Mark the visited cells
+            temp = board[r][c]
             board[r][c] = "X"
-
-            cur = dfs(r+1,c,i+1) or dfs(r-1,c,i+1) or dfs(r,c+1,i+1) or dfs(r,c-1,i+1)
-            board[r][c] = tmp
-            return cur
-        
+            res = backtrack(r+1,c,i+1) or backtrack(r-1,c,i+1) or backtrack(r,c+1,i+1) or backtrack(r,c-1,i+1)
+            board[r][c] = temp
+            return res
         for r in range(rows):
             for c in range(cols):
-                if dfs(r,c,0):
+                if backtrack(r,c,0):
                     return True
         return False
