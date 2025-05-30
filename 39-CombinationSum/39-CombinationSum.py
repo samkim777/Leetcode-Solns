25bfa61c7a51dd18cf15005a045519201fdc630a
Sam <51801821+samkim777@users.noreply.github.com>
2025-05-29 22:05:12 -0700

    File updated at 5/29/2025, 10:05:12 PM

--- 39-CombinationSum/39-CombinationSum.py
+++ 39-CombinationSum/39-CombinationSum.py
@@ -1,4 +1,4 @@
-# Last updated: 5/7/2025, 10:22:53 PM
+# Last updated: 5/29/2025, 10:05:12 PM
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         res = []
@@ -6,13 +6,17 @@ class Solution:
             if target == curSum:
                 res.append(cur.copy())
                 return
-            if i >= len(candidates) or target < curSum:
+            
+            if target < curSum or i >= len(candidates):
                 return
             
             cur.append(candidates[i])
-            backtrack(i,curSum + candidates[i], cur)
+            backtrack(i, curSum + candidates[i], cur)
 
             cur.pop()
+            while i > 0 and candidates[i] == candidates[i-1]:
+                i += 1
             backtrack(i+1, curSum, cur)
+
         backtrack(0,0,[])
         return res
