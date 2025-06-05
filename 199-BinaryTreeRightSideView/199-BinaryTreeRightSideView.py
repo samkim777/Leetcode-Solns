307d0398220133d0c735d078256f0365b721615d
Sam <51801821+samkim777@users.noreply.github.com>
2025-06-04 22:30:42 -0700

    File updated at 6/4/2025, 10:30:42 PM

--- 199-BinaryTreeRightSideView/199-BinaryTreeRightSideView.py
+++ 199-BinaryTreeRightSideView/199-BinaryTreeRightSideView.py
@@ -1,4 +1,4 @@
-# Last updated: 5/24/2025, 10:53:20 PM
+# Last updated: 6/4/2025, 10:30:42 PM
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -7,24 +7,24 @@
 #         self.right = right
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-        # use bfs to traverse through the binary tree
-        # O(n) time and space complexity
         if not root:
             return []
         res = []
         queue = deque([root])
-
         while queue:
-            level = len(queue)
+            lvl = len(queue)
 
-            for i in range(level):
+            for i in range(lvl):
                 cur = queue.popleft()
-
-                if i == level - 1: # visited all the nodes in this level
+                
+                if i == lvl - 1:
                     res.append(cur.val)
                 
                 if cur.left:
                     queue.append(cur.left)
+                
                 if cur.right:
                     queue.append(cur.right)
         return res
+
+        
