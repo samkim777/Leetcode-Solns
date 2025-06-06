9bf0a2d506b04a8336552f00ec91026426afc224
Sam <51801821+samkim777@users.noreply.github.com>
2025-06-05 23:08:48 -0700

    File updated at 6/5/2025, 11:08:48 PM

--- 662-MaximumWidthofBinaryTree/662-MaximumWidthofBinaryTree.py
+++ 662-MaximumWidthofBinaryTree/662-MaximumWidthofBinaryTree.py
@@ -1,4 +1,4 @@
-# Last updated: 6/1/2025, 11:30:20 PM
+# Last updated: 6/5/2025, 11:08:48 PM
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -7,22 +7,22 @@
 #         self.right = right
 class Solution:
     def widthOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        queue = deque([(root,0)])
         res = 0
-
+        queue = deque([(root,0)])
         while queue:
-            right = -1
+            rightPos = -1
             lvl = len(queue)
-            _, left = queue[0]
+            _, idx = queue[0]
 
-            for i in range(lvl):
-                node, pos = queue.popleft()
-                if i == lvl - 1:
-                    right = pos
+            for j in range(lvl):
+                node,iCur = queue.popleft()
+                if j == lvl - 1:
+                    rightPos = iCur
                 if node.left:
-                    queue.append((node.left, 2* pos))
+                    queue.append((node.left, iCur * 2 ))
+                
                 if node.right:
-                    queue.append((node.right, 2* pos + 1))
-            
-            res = max(res, right - left + 1)
+                    queue.append((node.right, iCur * 2 + 1))
+            res = max(res, rightPos - idx + 1)
         return res
+                
