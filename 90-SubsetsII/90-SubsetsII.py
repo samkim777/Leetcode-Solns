29d93df4dc35b21ff878b06deb2e6bdd7334833b
Sam <51801821+samkim777@users.noreply.github.com>
2025-06-06 22:42:32 -0700

    File updated at 6/6/2025, 10:42:31 PM

--- 90-SubsetsII/90-SubsetsII.py
+++ 90-SubsetsII/90-SubsetsII.py
@@ -1,19 +1,27 @@
-# Last updated: 6/6/2025, 10:41:40 PM
+# Last updated: 6/6/2025, 10:42:31 PM
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
-        res = []
+        # Because our nums array contains duplicates, this means that
+        # in our decision tree, the decision to not include an element
+        # will require an extra step to ensure that we do not
+        # contain duplicate power sets
         nums.sort()
+        res = []
+
         def backtrack(i,cur):
             if i >= len(nums):
                 res.append(cur.copy())
                 return
             
+            # decision tree to add
             cur.append(nums[i])
             backtrack(i+1, cur)
 
+            # Decision to not add
             cur.pop()
-            while i < len(nums) - 1 and nums[i] == nums[i+1]:
-                i += 1
+            # We do not want to recurse if already used the number
+            while i < len(nums)- 1 and nums[i] == nums[i + 1]:
+                i+= 1
             backtrack(i+1, cur)
         backtrack(0,[])
         return res
