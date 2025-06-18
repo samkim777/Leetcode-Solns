ec9754499d6a6573bd04d2cedcf7596277326bd9
Sam <51801821+samkim777@users.noreply.github.com>
2025-06-17 22:31:04 -0700

    File updated at 6/17/2025, 10:31:04 PM

--- 994-RottingOranges/994-RottingOranges.py
+++ 994-RottingOranges/994-RottingOranges.py
@@ -1,39 +1,27 @@
-# Last updated: 6/17/2025, 10:30:30 PM
+# Last updated: 6/17/2025, 10:31:04 PM
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        if not grid:
-            return -1
-        directions = [(1,0), (-1,0), (0,-1), (0,1)]
-        rows,cols = len(grid), len(grid[0])
-        fresh_oranges = 0
-        time = 0
+        minutes = 0
+        oranges = 0
+        rows, cols = len(grid), len(grid[0])
+        directions = [(1,0), (-1,0), (0,1), (0,-1)]
         queue = deque()
-        
-        # First find position of rotten oranges
+
         for r in range(rows):
             for c in range(cols):
-                if grid[r][c] == 2:
+                if grid[r][c] == 1: # Fresh
+                    oranges += 1
+                elif grid[r][c] == 2:
                     queue.append((r,c))
-                elif grid[r][c] == 1:
-                    fresh_oranges += 1
         
-        while queue and fresh_oranges > 0:
-            time += 1
-            level_size = len(queue)
-
-            # Check the 4 directions from currently rotten oranges
-            for _ in range(level_size):
+        while queue and oranges:
+            minutes += 1
+            for _ in range(len(queue)):
                 row, col = queue.popleft()
-
                 for dr, dc in directions:
-                    curRow = row + dr
-                    curCol = col + dc
-
-                    # Check within bounds and fresh
-                    # don't need visited set because we are marking the grid as we traverse
+                    curRow, curCol = row + dr, col + dc
                     if 0 <= curRow < rows and 0 <= curCol < cols and grid[curRow][curCol] == 1:
-                        grid[curRow][curCol] = 2
-                        fresh_oranges -= 1
                         queue.append((curRow,curCol))
-        return time if fresh_oranges == 0 else -1
-
+                        grid[curRow][curCol] = 2
+                        oranges -= 1
+        return minutes if oranges == 0 else -1
