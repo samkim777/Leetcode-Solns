ff0188012a25ecfc2c2fa108094e06f5402748d7
Sam <51801821+samkim777@users.noreply.github.com>
2025-06-19 22:15:48 -0700

    File updated at 6/19/2025, 10:15:48 PM

--- 815-BusRoutes/815-BusRoutes.py
+++ 815-BusRoutes/815-BusRoutes.py
@@ -1,38 +1,34 @@
-# Last updated: 6/19/2025, 10:05:39 PM
+# Last updated: 6/19/2025, 10:15:48 PM
 class Solution:
     def numBusesToDestination(self, routes: List[List[int]], source: int, target: int) -> int:
         if source == target:
             return 0
-        
-        bus_stops = {}
-        # Construct Adjacency List
-        # Key == route
-        # Value == Index of route that contains this stop
-        # {0:[1,2,3]} Means 0 index route has destinations 1 2 3
-        for i, route in enumerate(routes):
+        # create adjacency list
+        bus_stops = defaultdict(list)
+        for index, route in enumerate(routes):
             for stop in route:
-                if stop not in bus_stops:
-                    bus_stops[stop] = []
-                bus_stops[stop].append(i)
-        
-        visited = set()  # visited buses only
-        queue = deque()
+                bus_stops[stop].append(index) # {route1: index of route array that contains route 1, 
+            # route2: index of route array that contains route 2...}
 
+        # Edge case for when our source or target is not inside any routes
         if source not in bus_stops or target not in bus_stops:
-            return -1 
+            return -1
+        queue = deque()
+        visited = set()
 
+        # Start off queue from source --> We know source is in our routes
         for bus in bus_stops[source]:
-            queue.append((bus, 1))
+            queue.append((bus,1))
             visited.add(bus)
-        while queue:
-            currBus, numBus = queue.popleft()
 
-            for stop in routes[currBus]:
-                if stop == target:
-                    return numBus
-                for connectedBus in bus_stops[stop]:
+        while queue:
+            curidx, curBusNums = queue.popleft() # ex: 1, 3: index of route 1, 3 buses req
+
+            for route in routes[curidx]:
+                if route == target:
+                    return curBusNums
+                for connectedBus in bus_stops[route]:
                     if connectedBus not in visited:
+                        queue.append((connectedBus, curBusNums + 1))
                         visited.add(connectedBus)
-                        queue.append((connectedBus, numBus + 1))
-        
         return -1
