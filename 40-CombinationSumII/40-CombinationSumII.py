ac882e1e5a67022335f67a9cec0913daf710f1a1
Sam <51801821+samkim777@users.noreply.github.com>
2025-06-22 22:43:26 -0700

    File updated at 6/22/2025, 10:43:26 PM

--- 40-CombinationSumII/40-CombinationSumII.py
+++ 40-CombinationSumII/40-CombinationSumII.py
@@ -1,30 +1,31 @@
-# Last updated: 5/11/2025, 10:49:38 PM
+# Last updated: 6/22/2025, 10:43:26 PM
 class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
-        candidates.sort()
         res = []
+        # Sort first so that when we make a decision to add or not to add,
+        # we can skip accordingly. Does not affect overall time or space complexity
+        candidates.sort()
 
-        def backtrack(i,curVal, curArr):
-            # base case: Found target
+        def backtrack(i, curVal, curArr):
+            # Found
             if target == curVal:
-                res.append(curArr.copy()) # We add a shallow copy
-                # since it is being passed through recursion
+                res.append(curArr.copy())
                 return
-            # target too small with current sum
-            # index out of array
+            # Base case
             if i >= len(candidates) or target < curVal:
                 return
             # Decision to add
             curArr.append(candidates[i])
-            backtrack(i+1, curVal + candidates[i], curArr)
+            backtrack(i + 1, curVal + candidates[i], curArr)
 
-            # Decision to not add
+            # Decision to skip
+            # here we need to move our index accordingly so that we don't
+            # add combinations with the same number twice
+            # ex: [1,1,2] and [1,1,2] may use different numbers in terms of reference
+            # but are same numbers
             curArr.pop()
-            # Skip duplicate
             while i < len(candidates) - 1 and candidates[i] == candidates[i+1]:
-                i+=1
+                i+= 1
             backtrack(i+1, curVal, curArr)
-            # we don't need to add curVal with another value because it'll 
-            # recurse up anyways to handle when we follow "add" decision
         backtrack(0,0,[])
         return res
