f38d5c31cc5de4fdcccfb622d51a64792c23f4f5
Sam <51801821+samkim777@users.noreply.github.com>
2025-06-27 05:22:50 -0700

    File updated at 6/27/2025, 5:22:50 AM

--- 79-WordSearch/79-WordSearch.py
+++ 79-WordSearch/79-WordSearch.py
@@ -1,22 +1,38 @@
-# Last updated: 5/19/2025, 11:11:56 PM
+# Last updated: 6/27/2025, 5:22:50 AM
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
+        # We can use backtrackiing to recursively go through this
+        # matrix, and if the current character is inside our string
+        # then we can continue
+        # else recurse
+        # At each grid, check 4 directions and check recursively
+        # Time complexity: O(m*n)
+        # Space complexity: O(m*n)
+        if not board:
+            return False
+        res = False
+        directions = [(1,0), (-1,0), (0,1), (0,-1)]
         rows, cols = len(board), len(board[0])
+
         def backtrack(r,c,i):
-            # base case when we find the char
-            if i == len(word):
+            if i == len(word): # Char not found
                 return True
-            # base case for when if out of bounds or not correct char
-            if r < 0 or r >= rows or c < 0 or c >= cols or word[i] != board[r][c]:
+            if r >= rows or r < 0 or c >= cols or c < 0 or word[i] != board[r][c]:
                 return False
-            # Mark the visited cells
-            temp = board[r][c]
-            board[r][c] = "X"
-            res = backtrack(r+1,c,i+1) or backtrack(r-1,c,i+1) or backtrack(r,c+1,i+1) or backtrack(r,c-1,i+1)
-            board[r][c] = temp
-            return res
+            
+            tmp = board[r][c]
+            board[r][c] = "#"
+            
+            for dr, dc in directions:
+                if backtrack(r+dr, c+dc, i+1):
+                    return True
+            
+            board[r][c] = tmp
+            return False
+        
         for r in range(rows):
             for c in range(cols):
                 if backtrack(r,c,0):
                     return True
         return False
+            
