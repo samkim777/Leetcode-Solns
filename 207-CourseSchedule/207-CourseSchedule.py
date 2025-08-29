79044ca4c1206f2a0a02c2197ef9d2270756fb1c
Sam <51801821+samkim777@users.noreply.github.com>
2025-08-29 06:25:31 -0700

    File updated at 8/29/2025, 6:25:31 AM

--- 207-CourseSchedule/207-CourseSchedule.py
+++ 207-CourseSchedule/207-CourseSchedule.py
@@ -1,28 +1,30 @@
-# Last updated: 8/29/2025, 12:40:33 AM
+# Last updated: 8/29/2025, 6:25:31 AM
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        premap = {i :[] for i in range(numCourses)}
+        # 0 <= a_i, b_i < numCourses
+        precourse = {i:[] for i in range(numCourses)}
         visited = set()
 
         for course, prereq in prerequisites:
-            premap[course].append(prereq)
+            precourse[course].append(prereq)
         
-        # perform cycle detection using dfs
-        # If the pre requisite course is a course we have to take, then it's doom
         def dfs(course):
-            if course in visited:
+            if course in visited: # Cycle
                 return False
-            
-            if premap[course] == []:
+            # We'll be popping from the prereq array of each course, so the base case is
+            # when our course has an empty array
+            if precourse[course] == []:
                 return True
             
             visited.add(course)
 
-            for pre in premap[course]:
-                if not dfs(pre):
+            # Traverse through each element in the pre req
+            for prereq in precourse[course]:
+                if not dfs(prereq):
                     return False
+            
             visited.remove(course)
-            premap[course] = []
+            precourse[course] = []
             return True
         for courses in range(numCourses):
             if not dfs(courses):
