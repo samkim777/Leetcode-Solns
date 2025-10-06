83da7f4c836279333af007ee5f451f07787f2f3b
Sam <51801821+samkim777@users.noreply.github.com>
2025-10-06 17:38:12 +0900

    File updated at 10/6/2025, 5:38:11 PM

--- 200-NumberofIslands/200-NumberofIslands.py
+++ 200-NumberofIslands/200-NumberofIslands.py
@@ -1,19 +1,18 @@
-# Last updated: 10/6/2025, 5:31:32 PM
+# Last updated: 10/6/2025, 5:38:11 PM
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         rows, cols = len(grid), len(grid[0])
+
         islands = 0
 
         def dfs(i,j):
-            # Base case
             if i < 0 or i >= rows or j < 0 or j >= cols or grid[i][j] == '0':
                 return
-            
             grid[i][j] = '0'
-            dfs(i,j+1)
-            dfs(i,j-1)
             dfs(i+1,j)
             dfs(i-1,j)
+            dfs(i,j+1)
+            dfs(i,j-1)
         
         for r in range(rows):
             for c in range(cols):
