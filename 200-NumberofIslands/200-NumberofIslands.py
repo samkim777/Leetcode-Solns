0a4bdac144806e3c922ebb92d7047664e02fa345
Sam <51801821+samkim777@users.noreply.github.com>
2025-10-06 16:57:55 +0900

    File updated at 10/6/2025, 4:57:55 PM

--- 200-NumberofIslands/200-NumberofIslands.py
+++ 200-NumberofIslands/200-NumberofIslands.py
@@ -1,23 +1,25 @@
-# Last updated: 7/6/2025, 7:15:38 AM
+# Last updated: 10/6/2025, 4:57:55 PM
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        rows, cols = len(grid), len(grid[0])
-        directions = [(1,0), (-1,0), (0,1), (0,-1)]
+        # Time complexity : O(m*n)
+        # Space complexity: O(m*n)
         queue = deque()
-        visited = set()
-        numIslands = 0
+        islands = 0
+        rows, cols = len(grid), len(grid[0])
+        directions = [[1,0],[-1,0],[0,1],[0,-1]]
 
-        for r in range(rows):
-            for c in range(cols):
-                if grid[r][c] == "1" and (r,c) not in visited: # Land
-                    queue.append((r,c))
-                    visited.add((r,c))
+        for row in range(rows):
+            for col in range(cols):
+                if grid[row][col] == "1":
+                    queue.append((row,col))
+                    grid[row][col] = "0"
                     while queue:
-                        row, col = queue.popleft()
-                        for dr, dc in directions:
-                            curRow, curCol = row + dr, col + dc
-                            if curRow < rows and curRow >= 0 and curCol >= 0 and curCol < cols and grid[curRow][curCol] == "1" and (curRow,curCol) not in visited:
-                                queue.append((curRow,curCol))
-                                visited.add((curRow,curCol))
-                    numIslands += 1
-        return numIslands
+                        r,c = queue.popleft()
+                        for dr,dc in directions:
+                            curRow,curCol = r+dr,c+dc
+                            if 0 <= curRow < rows and 0 <= curCol < cols and grid[curRow][curCol] == "1":
+                                queue.append([curRow,curCol])
+                                grid[curRow][curCol] = "0"
+                    islands += 1
+        return islands
+            
