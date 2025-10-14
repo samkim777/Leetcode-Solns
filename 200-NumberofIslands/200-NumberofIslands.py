e91b2b951f28730ec10cbec6196006b33fe9940b
Sam <51801821+samkim777@users.noreply.github.com>
2025-10-14 23:58:18 +0900

    File updated at 10/14/2025, 11:58:18 PM

--- 200-NumberofIslands/200-NumberofIslands.py
+++ 200-NumberofIslands/200-NumberofIslands.py
@@ -1,22 +1,34 @@
-# Last updated: 10/14/2025, 11:53:08 PM
+# Last updated: 10/14/2025, 11:58:18 PM
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+        # BFS immediately comes to mind with these 2D grid problems
+        # When we meet land, then we can add that to our queue
+        # And mark it as water to make sure we don't visit it again
+        # Standard bfs approach
+        # Time complexity will be O(m*n) because we must traverse the
+        # entire grid
+        # Space complexity will be O(m*n) because in worst case scenario
+        # when we have all lands, our queue will be size of the grid
+        # Assuming m,n >= 1
+        queue = deque()
         rows, cols = len(grid), len(grid[0])
+        directions = [[1,0],[-1,0],[0,1],[0,-1]]
+        numIslands = 0
 
-        islands = 0
-
-        def dfs(i,j):
-            if i < 0 or i >= rows or j < 0 or j >= cols or grid[i][j] == '0':
-                return
-            grid[i][j] = '0'
-            dfs(i+1,j)
-            dfs(i-1,j)
-            dfs(i,j+1)
-            dfs(i,j-1)
-        
         for r in range(rows):
             for c in range(cols):
-                if grid[r][c] == '1':
-                    islands += 1
-                    dfs(r,c)
-        return islands
+                if grid[r][c] == "1": # If watuh, add to queue
+                    queue.append((r,c))
+                    grid[r][c] = "0"
+                    while queue:
+                        row,col = queue.popleft() # get current land
+                        for dr, dc in directions:
+                            curRow, curCol = row + dr, col + dc
+                            # Check validity
+                            if curRow < 0 or curRow >= rows or curCol < 0 or curCol >= cols or grid[curRow][curCol] == "0":
+                                continue
+                            grid[curRow][curCol] = "0"
+                            queue.append((curRow,curCol))
+                    numIslands += 1
+        return numIslands
+
